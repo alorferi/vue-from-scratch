a414e40579a3acfca344602f0ff44d88d42885e4
Babul Mirdha <babumirdha@gmail.com>
2020-06-08 03:34:07 +0600

    done e10

---
+++ src/components/Task.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul>
+        <li>Item 1</li>
+         <li>Item 2</li>
+          <li>Item 3</li>
+    </ul>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus">
+
+</style>
